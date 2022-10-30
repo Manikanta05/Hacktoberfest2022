0d1e0d34d301a7d80d15f25964777a2a6bc7d599
Akhil Harwadekar <61285173+Akhil-Harwadekar@users.noreply.github.com>
2022-10-31 01:56:25 +0530

    Update TodoRepository.java

--- Todo_application/src/main/java/com/mani/todo/repository/TodoRepository.java
+++ Todo_application/src/main/java/com/mani/todo/repository/TodoRepository.java
@@ -1,5 +1,11 @@
 package com.mani.todo.repository;
 
-public interface TodoRepository {
-	// ADD your code here
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mani.todo.model.TodoModel;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoModel, Integer> {
+
 }
